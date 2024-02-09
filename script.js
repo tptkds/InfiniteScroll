fff47dda1c4a0d7a3465d0d8bc84d458dbf84db7
KimYouGyoung <tptkds12@gmail.com>
2024-02-10 00:32:20 +0900

    데이터가져오기 구현 완료

--- script.js
+++ script.js
@@ -4,4 +4,38 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $posts = get('.posts');
+
+  const getPost = async () => {
+    const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+    const reponse = await fetch(API_URL);
+    if (!reponse.ok) {
+      throw new Error('에러');
+    }
+    return await reponse.json();
+  }
+
+  const showPosts = (posts) => {
+    posts.forEach((post) => {
+      const $post = document.createElement('div');
+      $post.classList.add('post');
+      $post.innerHTML = `
+      <div class="header">
+        <div class="id">${post.id}</div>
+        <div class="title">${post.title}</div>
+      </div>
+      <div class="body">${post.body}</div>`;
+      $posts.appendChild($post);
+    })
+  }
+
+  const loadPost = async () => {
+    const response = await getPost();
+    showPosts(response);
+  }
+
+  window.addEventListener('DOMContentLoaded', () => {
+    loadPost();
+  })
 })()
